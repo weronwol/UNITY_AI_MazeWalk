6ee7814618ae39cd689839ef9cd34cd585d97099
weronwol <55180739+weronwol@users.noreply.github.com>
2020-04-28 22:04:48 +0800

    428

---
+++ UNITY_AI_MazeWalker/Assets/AiWalking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiWalking : MonoBehaviour
+{
+    public NavMeshAgent agent;    //宣告NavMeshAgent
+    public GameObject target;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
